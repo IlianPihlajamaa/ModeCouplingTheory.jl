636a389858bbe02bd3d85853650f68a37be10ecc
Ilian Pihlajamaa <ilian.pihlajamaa@gmail.com>
2022-11-02 18:22:56 +0100

    Provide better error messages

--- src/Kernels.jl
+++ src/Kernels.jl
@@ -11,7 +11,7 @@ include("Kernels/MultiComponentKernels.jl")
 Evaluates the memory kernel in-place, overwriting the elements of the `out` variable. It may mutate the content of `kernel`.
 """
 function evaluate_kernel!(out, kernel::MemoryKernel, F, t)
-    error("This memory kernel is not defined")
+    error("This memory kernel is not defined. This means that you are probably trying to call `evaluate_kernel` with a kernel type for which this function has no specific method.")
 end
 
 
@@ -24,5 +24,5 @@ Returns
 * `out` the kernel evaluated at (F, t)
 """
 function evaluate_kernel(kernel::MemoryKernel, F, t)
-    error("This memory kernel is not defined")
+    error("This memory kernel is not defined. This means that you are probably trying to call `evaluate_kernel` with a kernel type for which this function has no specific method.")
 end
